1ce106a269bd5bf7e7df66013a2c6aa0ade87934
Angel_zapata <22170124@uttcampus.edu.mx>
2023-06-12 22:46:15 -0600

    agregandole poco mas

--- veu/mi_primer_veo/src/App.vue
+++ veu/mi_primer_veo/src/App.vue
@@ -1,22 +1,27 @@
+<script setup>
+const name  = " soy una sobre escritura "
+const stylecolor = "color: blue"
+const arry=['blue','red','peru'];
+</script>
 <template>
 <div class="container">
-  hola
+  <h1 >hola<!--{{ name }}--></h1> 
+  <h2 :style=stylecolor>pero azul</h2>
+   
   <button 
   class="btn btn-primary" 
   data-bs-target="#collapseTarget" 
   data-bs-toggle="collapse">
-  Bootstrap collapse
+  soy un boton
 </button>
 <div class="collapse py-2" id="collapseTarget">
-  This is the toggle-able content!
+  y yo un gato we
 </div>
 </div>
 </template>
 
-<script>
-
-</script>
-
 <style>
-
+h1{
+  color: red;
+}
 </style>

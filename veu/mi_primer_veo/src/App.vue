10dc0dad5e095a1019ab6b3525ad6291236e2821
Angel_zapata <22170124@uttcampus.edu.mx>
2023-06-12 20:20:21 -0600

    dando stylosxd

--- veu/mi_primer_veo/src/App.vue
+++ veu/mi_primer_veo/src/App.vue
@@ -1,6 +1,15 @@
 <template>
 <div class="container">
   hola
+  <button 
+  class="btn btn-primary" 
+  data-bs-target="#collapseTarget" 
+  data-bs-toggle="collapse">
+  Bootstrap collapse
+</button>
+<div class="collapse py-2" id="collapseTarget">
+  This is the toggle-able content!
+</div>
 </div>
 </template>
 
--- veu/mi_primer_veo/src/main.js
+++ veu/mi_primer_veo/src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 createApp(App).mount('#app')

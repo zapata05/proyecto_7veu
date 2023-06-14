7cfe8c82750c196e8f44140416ff954ac0f34444
Angel_zapata <22170124@uttcampus.edu.mx>
2023-06-14 17:44:16 -0600

    nombre padre echale ganas en la escuela te falto mucho y aqui mas

--- veu/mi_primer_veo/src/App.vue
+++ veu/mi_primer_veo/src/App.vue
@@ -1,51 +1,178 @@
-<script setup>
-import {ref} from 'vue'
-const name  = " soy una sobre escritura "
-const stylecolor = "color: blue"
-const arry=['blue','red','peru'];
-
-const cambiar=ref['-','-','-','-','-','-','-','-','-'];
-
-const camb =()=>{
-  cambiar.value
-}
-</script>
 <template>
-<div class="container end">
-  <h1 >hola<!--{{ name }}--></h1> 
-  <h2 :style=stylecolor>pero azul</h2>
-   
-  <button 
-  class="btn btn-primary" 
-  data-bs-target="#collapseTarget" 
-  data-bs-toggle="collapse">
-  soy un boton
-</button>
-<div class="collapse py-2" id="collapseTarget">
-  y yo un gato we
-</div>
-</div>
-<div class="container1 bg-danger vh-400 p-2">
-  <div class="row m-2 grid text-center ">
-    <button class="col btn btn-primary btn-lg btn-block" @click="camb" ></button>
-    <button class="col btn btn-primary btn-lg btn-block" @click="camb" ></button>
-    <button class="col btn btn-primary btn-lg btn-block" @click="camb"></button>
+  
+  <div class="game">
+    <h1 >iniciar el juego perron del #</h1>
+    <div class="scoreboard">
+      <div class="player-score">
+        Jugador X: {{ xScore }}
+      </div>
+      <div class="player-score">
+        Jugador O: {{ oScore }}
+      </div>
+    </div>
+    <div class="board">
+      <div
+        class="cell"
+        v-for="(cell, index) in cells"
+        :key="index"
+        @click="handleCellClick(index)"
+        :class="{ 'cell-selected': cell !== '' }"
+      >
+        {{ cell }}
+      </div>
+    </div>
+    <div class="message" v-if="winner">
+      ¡{{ winner }} ha ganado el juego!
+    </div>
+    <div class="button-container">
+      <button @click="resetGame" :disabled="!winner">Reiniciar</button>
+    </div>
   </div>
-  <div class="row m-2 grid text-center">
-    <button class="col btn btn-primary btn-lg btn-block" @click="camb" ></button>
-    <button class="col btn btn-primary btn-lg btn-block" @click="camb" ></button>
-    <button class="col btn btn-primary btn-lg btn-block" @click="camb"></button>
-  </div>
-  <div class="row m-2 grid text-center">
-    <button class="col btn btn-primary btn-lg btn-block" @click="camb" ></button>
-    <button class="col btn btn-primary btn-lg btn-block" @click="camb" ></button>
-    <button class="col btn btn-primary btn-lg btn-block" @click="camb"></button>
-  </div>
-</div>
 </template>
 
-<style>  
-h1{
-  color: red;
+<script>
+export default {
+  data() {
+    return {
+      cells: Array(9).fill(""),
+      currentPlayer: "X",
+      winner: null,
+      winningCombinations: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ],
+      xScore: 0,
+      oScore: 0
+    };
+  },
+  methods: {
+    handleCellClick(index) {
+      if (!this.cells[index] && !this.winner && !this.isBoardFull()) {
+        this.cells[index] = this.currentPlayer;
+        this.checkWinner();
+        this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+      }
+    },
+    checkWinner() {
+      for (let combination of this.winningCombinations) {
+        const [a, b, c] = combination;
+        if (
+          this.cells[a] &&
+          this.cells[a] === this.cells[b] &&
+          this.cells[a] === this.cells[c]
+        ) {
+          this.winner = this.cells[a];
+          this.updateScore();
+          break;
+        }
+      }
+      if (!this.winner && this.isBoardFull()) {
+        this.winner = "Empate";
+      }
+    },
+    updateScore() {
+      if (this.winner === "X") {
+        this.xScore++;
+      } else if (this.winner === "O") {
+        this.oScore++;
+      }
+    },
+    isBoardFull() {
+      return this.cells.every(cell => cell !== "");
+    },
+    resetGame() {
+      this.cells = Array(9).fill("");
+      this.currentPlayer = "X";
+      this.winner = null;
+    }
+  }
+};
+</script>
+
+<style>
+/* Estilos adicionales para el marcador */
+.scoreboard {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 20px;
+  font-size: 18px;
+}
+
+.player-score {
+  margin-right: 20px;
+}
+
+/* Estilos previos mantenidos */
+.game {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+
+.h1 {
+  display: flex;
+  align-items: center;
+  align-content: center;
+}
+
+.board {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 10px;
+  width: 300px;
+  height: 300px;
+  background-color: #fff;
+  border: 1px solid #333;
+  padding: 10px;
+}
+
+.cell {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #eee;
+  border: 1px solid #ccc;
+  font-size: 48px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.cell-selected {
+  background-color: #ffa500;
+  color: #fff;
+}
+
+.cell:hover:not(.cell-selected) {
+  background-color: #ddd;
+}
+
+.cell.disabled {
+  cursor: not-allowed;
+  background-color: #ccc;
+}
+
+.button-container {
+  margin-top: 20px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 18px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+}
+
+.message {
+  margin-top: 20px;
+  font-size: 24px;
 }
 </style>
